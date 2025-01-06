d9729850397ce922723e946b26cfa8092f54a3b4
unknown <abu040810@gmail.com>
2025-01-06 16:22:37 +0500

    new object method

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const car = {type: "Chevrolet",model: "Malibu",color: "pink"};
+//const car = {type: "Chevrolet",model: "Malibu",color: "pink"};
+//document.getElementById("demo").innerHTML =
+ //"Bu mashinani rangi " + car.color;
+ 
+
+
+
+ const car =new Object();
+ car.type="Malibu";
+ car.model="LT";
+ car.color="pink";
+ car.since="2024";
+
+
 document.getElementById("demo").innerHTML =
-    "Bu mashinani rangi " + car.color;
+ "Bu mashinani yili " + car.since;
