052c07d53279fee8242cef28825c7e68c60a887b
unknown <abu040810@gmail.com>
2025-01-08 16:09:02 +0500

    assign method

--- script.js
+++ script.js
@@ -15,15 +15,32 @@
 //document.getElementById("demo").innerHTML =
  //"Bu mashinani yili " + car.since;
 
- function Person(first, last, age, eye) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eye;
-  }
+ //function Person(first, last, age, eye) {
+ //   this.firstName = first;
+ //   this.lastName = last;
+  //  this.age = age;
+ //   this.eyeColor = eye;
+ // }
   
-  Person.prototype.gender = "male";
+ // Person.prototype.gender = "male";
   
-  const myFather = new Person("John", "Doe", 50, "blue");
-  document.getElementById("demo").innerHTML =
-  "The nationality of my father is " + myFather.gender; 
+ // const myFather = new Person("John", "Doe", 50, "blue");
+ // document.getElementById("demo").innerHTML =
+ // "The nationality of my father is " + myFather.gender; 
+
+
+ const sneakers1 = {
+  firstName: "Nike",
+  lastName: "airmax",
+  price: 100,
+  Color: "mixed"
+};
+
+
+const sneakers2 = {firstName: "Adidas",lastName: "Yeezy"};
+
+Object.assign(sneakers1, sneakers2);
+
+
+let text = Object.entries(sneakers1);  // returns an array
+document.getElementById("demo").innerHTML = text;
