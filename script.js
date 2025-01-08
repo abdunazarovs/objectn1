1d455ee2d757c6299bf5ffe11f402b036251b64d
unknown <abu040810@gmail.com>
2025-01-08 15:55:37 +0500

    prototype property

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const car = {type: "Chevrolet",model: "Malibu",color: "pink"};
-document.getElementById("demo").innerHTML =
- "Bu mashinani rangi " + car.color;
+//const car = {type: "Chevrolet",model: "Malibu",color: "pink"};
+//document.getElementById("demo").innerHTML =
+ //"Bu mashinani rangi " + car.color;
  
 
 
 
- const car =new Object();
- car.type="Malibu";
- car.model="LT";
- car.color="pink";
- car.since="2024";
+ //const car =new Object();
+ //car.type="Malibu";
+ //car.model="LT";
+ //car.color="pink";
+ //car.since="2024";
 
 
-document.getElementById("demo").innerHTML =
- "Bu mashinani yili " + car.since;
+//document.getElementById("demo").innerHTML =
+ //"Bu mashinani yili " + car.since;
+
+ function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+  
+  Person.prototype.gender = "male";
+  
+  const myFather = new Person("John", "Doe", 50, "blue");
+  document.getElementById("demo").innerHTML =
+  "The nationality of my father is " + myFather.gender; 
